d97915f780768720b6ffb77e1573da16ede7f87f
George <villegas.george96@gmail.com>
2024-10-19 01:36:13 -0700

    Objective2Lab2.java

---
+++ labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "George Villegas";
+		int age = 24;
+		String state = "California";
+		System.out.println(name + age + state);
+	}
+}
