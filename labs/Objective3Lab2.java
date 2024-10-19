c0a0f3ae4bc421599ea60025f58eeae888999c26
George <villegas.george96@gmail.com>
2024-10-19 03:11:59 -0700

    Objective3Lab2.java

---
+++ labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+    public static void main(String[] args) {
+          double num = Math.sqrt(3 * 3);    //num => 3
+    double result = Math.sqrt(4) + Math.sqrt(9); //result => 2 + 3 => 5
+    double side1 =8;
+    double side2 =10;
+    double hypotenuse = Math.sqrt(side1 * side1 + side2 * side2);
+    System.out.println(hypotenuse);
+    }
+}
