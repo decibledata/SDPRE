ffedd09514644853f486273138a075c6794c7d05
George <villegas.george96@gmail.com>
2024-10-19 01:54:01 -0700

    Objective2lab3.java

---
+++ labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year = 2000;
+    String month = "April";
+    boolean isWeekend = true;
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(isWeekend);
+  }
+}
