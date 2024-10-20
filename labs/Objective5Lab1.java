04e448ed355d51c9731b7f4c9769340bd28bb28a
George <villegas.george96@gmail.com>
2024-10-20 00:36:35 -0700

    objective5Lab1.java

---
+++ labs/Objective5Lab1.java
@@ -0,0 +1,19 @@
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+  
+
+    if(num1 > num2) {
+      System.out.println("5 is Greater than 15");
+    }
+    else if(num2 > num1) {
+      System.out.println("15 is greater than 5");
+
+      /*
+        Your solution goes here
+      */
+    }
+  }
+}
